6d1b5c195e012fb50fb22771821e992cdfeb0b0b
imedisa <medisayep@gmail.com>
2023-08-23 16:08:19 +0330

    edited

--- src/pages/country.vue
+++ src/pages/country.vue
@@ -8,25 +8,35 @@
           Back
         </button>
       </router-link>
-      <div class="grid sm:grid-cols-12 grid-cols-1 gap-x-8 gap-y-0 sm:gap-y-4 mt-9">
-        <div class="col-span-6">
+      <div
+        class="grid sm:grid-cols-12 grid-cols-1 gap-x-8 gap-y-0 sm:gap-y-4 mt-9"
+      >
+        <div class="col-span-6 ">
           <img
-            class="h-4/6 w-full object-cover"
+            class="h-5/6 lg:h-full w-full object-cover"
             :src="country_info?.imgUrl"
+            v-if="country_info?.imgUrl"
           />
         </div>
         <div class="col-span-6">
           <div class="flex flex-row justify-between w-11/12 mx-auto">
             <div class="">
               <!-- TITLE -->
-              <div class="font-bold text-dark md:text-3xl text-sm sm:text-lg">{{ country_info?.countryName }}</div>
+              <div class="font-bold text-black md:text-3xl text-sm sm:text-lg">
+                {{ country_info?.countryName }}
+              </div>
+              <!-- CONTENT -->
+              <div
+                class="font-medium text-sm sm:text-md md:text-xl text-dark mt-10"
+              >
+                Navite Name : <span class="text-gray-600 font-regular text-sm sm:text-md md:text-xl">{{ country_info?.nativeName }}</span>
+              </div>
             </div>
             <div class="">frf</div>
           </div>
           <div class="">fdf</div>
         </div>
       </div>
-      
     </div>
   </div>
 </template>
@@ -39,7 +49,8 @@ export default {
     return {
       country_info: {
         imgUrl: "",
-        countryName:"",
+        countryName: "",
+        nativeName: "",
       },
     };
   },
@@ -47,12 +58,14 @@ export default {
     async getCountry(country_name) {
       try {
         const httpResponse = await axios.get(
-          `https://restcountries.com/v3.1/name/${country_name}`
+          `https://restcountries.com/v3.1/name/${country_name}?fullText=true`
         );
+        console.log(httpResponse);
         if (httpResponse.data.length) {
           this.country_info = {
             imgUrl: httpResponse.data[0]?.flags?.png,
-            countryName : httpResponse.data[0]?.name?.common,
+            countryName: httpResponse.data[0]?.name?.common,
+            nativeName: httpResponse.data[0]?.name?.official,
           };
         }
       } catch (error) {
